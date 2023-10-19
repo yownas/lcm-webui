70c0674f089a5101025c8931a2b428b0f5be1916
Jonas Stare <yownas@gmail.com>
2023-10-19 18:58:37 +0200

    "Works"

--- webui.py
+++ webui.py
@@ -1,8 +1,11 @@
+import datetime
+import os
 import gradio
 import argparse
 import gradio as gr
 from diffusers import DiffusionPipeline
 import torch
+from PIL.PngImagePlugin import PngInfo
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -32,7 +35,7 @@ def launch(args, gradio_root):
         share=args.share,
     )
 
-def check(image, device, dtype):
+def or_nice(image, device, dtype):
     return image, None
 
 pipe = DiffusionPipeline.from_pretrained(
@@ -42,36 +45,76 @@ pipe = DiffusionPipeline.from_pretrained(
 )
 
 pipe.enable_xformers_memory_efficient_attention()
-# To save GPU memory, torch.float16 can be used, but it may compromise image quality.
 pipe.to(torch_device="cuda", torch_dtype=torch.float32)
 # Slow, low vram
 #pipe.enable_sequential_cpu_offload()
 if args.naughty:
-    pipe.run_safety_checker = check
+    pipe.run_safety_checker = or_nice
+
+def generate_temp_filename(index=1, folder="./outputs/", extension="png"):
+    current_time = datetime.datetime.now()
+    date_string = current_time.strftime("%Y-%m-%d")
+    time_string = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"{time_string}_{index}.{extension}"
+    result = os.path.join(folder, date_string, filename)
+    return os.path.abspath(os.path.realpath(result))
 
 def generate(prompt, steps, cfg, size, image_count):
     (width, height) = size.split('x')
-    images = pipe(
-        prompt=prompt,
-        num_inference_steps=int(steps),
-        guidance_scale=float(cfg),
-        lcm_origin_steps=50,
-        output_type="pil",
-        width=int(width),
-        height=int(height)
-    ).images
+    result = []
+    filename = ""
+    for i in range(image_count):
+        filename = generate_temp_filename(index=i+1)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        images = pipe(
+            prompt=prompt,
+            num_inference_steps=int(steps),
+            guidance_scale=float(cfg),
+            lcm_origin_steps=50,
+            output_type="pil",
+            width=int(width),
+            height=int(height)
+        ).images[0]
+        metadata = PngInfo()
+        metadata.add_text("parameters", f"prompt: {prompt}\n\nsteps: {steps}\ncfg: {cfg}\nwidth: {width} height: {height}")
+        images.save(filename, pnginfo=metadata)
+        result.append(filename)
 
     return {
-        image: gr.update(value=images[0]),
-        gallery: gr.update(),
+        image: gr.update(value=filename),
+        gallery: gr.update(value=result),
     }
+
+scripts = """
+function generate_shortcut(){
+  document.addEventListener('keydown', (e) => {
+    let handled = false;
+    if (e.key !== undefined) {
+      if ((e.key === 'Enter' && (e.metaKey || e.ctrlKey || e.altKey))) handled = true;
+    } else if (e.keyCode !== undefined) {
+      if ((e.keyCode === 13 && (e.metaKey || e.ctrlKey || e.altKey))) handled = true;
+    }
+    if (handled) {
+      const button = document.getElementById('generate');
+      if (button) button.click();
+      e.preventDefault();
+    }
+  });
+}
+"""
+
+css = """
+"""
 
 gradio_root = gr.Blocks(
     title="LCM webui",
+    theme=None,
+    css=css,
     analytics_enabled=False,
 ).queue()
 
 with gradio_root as block:
+    block.load(_js=scripts)
     with gr.Row():
         image = gr.Image(
             height=680,
@@ -133,6 +176,12 @@ with gradio_root as block:
             step=1,
             value=1,
         )
+
+    @gallery.select(
+        inputs=[gallery], outputs=[image], show_progress="hidden"
+    )
+    def gallery_change(files, sd: gr.SelectData):
+        return files[sd.index]["name"]
 
     submit.click(
         fn=generate,
